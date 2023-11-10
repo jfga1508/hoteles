3fbba693c53f787dd0c8f8dbb2b3e9cba6b5ad97
Jean <jfga1508@gmail.com>
2023-11-09 19:06:19 -0500

    deploy test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.scss';
 
 function App() {
-    const [count, setCount] = useState(0);
-
     return (
         <>
             <div>
@@ -20,10 +17,6 @@ function App() {
                     />
                 </a>
             </div>
-
-            <p className='read-the-docs'>
-                Click on the Vite and React logos to learn more
-            </p>
         </>
     );
 }
